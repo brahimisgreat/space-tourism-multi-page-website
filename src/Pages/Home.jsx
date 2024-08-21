f94351a283f5b143806b0fb2d7b6beb1c5ad13f8
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-08-20 21:00:10 -0400

    fddsf

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <>
-      <section className="home">
-        <div className="overlay">
+      <section className="home px-5">
+        <div className="overlay ">
           <article>
-            <h1>
-              SO, YOU WANT TO TRAVEL TO <span>space</span>
+            <h1 className=" text-2xl text-gray-400">
+              SO, YOU WANT TO TRAVEL TO <span className="block text-4xl lg:text-6xl mt-5 mb-10 ">SPACE</span>
             </h1>
-            <p>
+            <p className="text-gray-400">
               Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
@@ -17,8 +17,8 @@ export const Home = () => {
             </p>
           </article>
 
-          <article>
-            <button>
+          <article className="mt-20">
+            <button className="w-48 h-48 rounded-full bg-white text-gray-800  font-bold text-2xl lg:text-5xl animate-pulse " >
                 <Link to='/destination'>EXPLORE</Link>
             </button>
           </article>
