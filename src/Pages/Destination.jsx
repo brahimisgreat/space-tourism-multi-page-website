9ebd1e268384a7643a6bfdbb8444deb6329fa18d
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-08-25 01:52:52 -0400

    fdfd

--- src/Pages/Destination.jsx
+++ src/Pages/Destination.jsx
@@ -1,6 +1,65 @@
+import planets from "../data.json";
+import moon from "../assets/destination/image-moon.webp";
+import { NavLink } from "react-router-dom";
+import "./Moon.css";
 
 export const Destination = () => {
+  const planet = planets.destinations[0];
+
+  console.log(planet);
+
   return (
-    <div>Destination</div>
-  )
-}
+    <>
+      <div className="destinations-overlay">
+        <section className="Destinations">
+          <div>
+            <h3>
+              <span>01</span> PICK YOUR DESTINATION
+            </h3>
+
+            <img src={moon} alt="moon" />
+          </div>
+
+          <div className="explanations">
+            <div className="planets-nav">
+              <NavLink to="/" className="planet">
+                MOON
+              </NavLink>
+              <NavLink to="/" className="planet">
+                MARS
+              </NavLink>
+              <NavLink to="/" className="planet">
+                EUROPA
+              </NavLink>
+              <NavLink to="/" className="planet">
+                TITAN
+              </NavLink>
+            </div>
+
+            <h1>{planet.name}</h1>
+
+            <p>
+              See our planet as you’ve never seen it before. A perfect relaxing
+              trip away to help regain perspective and come back refreshed.
+              While you’re there, take in some history by visiting the Luna 2
+              and Apollo 11 landing sites.
+            </p>
+
+            <hr />
+
+            <div className="travel-time">
+              <div className="travel">
+                <span>AVG. DISTANCE</span>
+                <p>{planet.distance}</p>
+              </div>
+              <div className="time">
+                <span>EST TRAVEL TIME</span>
+                <p>{planet.travel}</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
