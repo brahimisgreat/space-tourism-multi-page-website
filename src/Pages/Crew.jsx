d3571f9d9486fe659518468abaca57480bbd2381
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-08-26 21:00:19 -0400

    fdf

--- src/Pages/Crew.jsx
+++ src/Pages/Crew.jsx
@@ -1,6 +1,31 @@
+import data from "../data.json";
+import "./Crew.css";
 
 export const Crew = () => {
   return (
-    <div>Crew</div>
-  )
-}
+    <>
+      <div className="crew-overlay">
+        <div>
+          <article className="box-1">
+            <h4>
+              <span>{}</span> MEET YOUR CREW
+            </h4>
+          </article>
+
+          <section className="box-2">
+            <h3></h3>
+            <h3></h3>
+          </section>
+
+          <section className="box-3">
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <img />
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
