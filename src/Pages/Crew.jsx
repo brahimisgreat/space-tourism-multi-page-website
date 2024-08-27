a2eec86aa391e2b4c42e3863e2e34e6e90f5d3b1
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-08-26 23:31:24 -0400

    ff

--- src/Pages/Crew.jsx
+++ src/Pages/Crew.jsx
@@ -1,11 +1,17 @@
 import data from "../data.json";
 import "./Crew.css";
 import { useState } from "react";
+import douglas from '../assets/crew/image-douglas-hurley.png'
+import mark from '../assets/crew/image-mark-shuttleworth.png'
+import victor from '../assets/crew/image-victor-glover.png'
+import anousheh from '../assets/crew/image-anousheh-ansari.png'
 
 export const Crew = () => {
 
 const [index, setIndex] = useState(0);
   const { crew } = data;
+
+  const crewImages = [douglas, mark, victor, anousheh];
 
   
 
@@ -17,7 +23,7 @@ const [index, setIndex] = useState(0);
         <div>
           <article className="box-1">
             <h4>
-              <span></span> MEET YOUR CREW
+              <span>02</span> MEET YOUR CREW
             </h4>
           </article>
 
@@ -30,9 +36,9 @@ const [index, setIndex] = useState(0);
           <section className="box-3">
             <button onClick={()=> setIndex(0)}>0</button>
             <button onClick={()=> setIndex(1)}>1</button>
-            <button onClick={()=> setIndex(2)}></button>
-            <button onClick={()=> setIndex(3)}></button>
-            <img />
+            <button onClick={()=> setIndex(2)}>2</button>
+            <button onClick={()=> setIndex(3)}>3</button>
+            <img  src={crewImages[index]}/>
           </section>
         </div>
       </div>
