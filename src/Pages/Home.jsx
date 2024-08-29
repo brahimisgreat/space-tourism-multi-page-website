48bf497e3488a34fcc1f81852bbcbdde41797ee1
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-08-28 20:28:51 -0400

    fdf

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 export const Home = () => {
 
@@ -7,14 +8,14 @@ export const Home = () => {
   }
   return (
     <>
-      <section className="home flex items-center px-5">
-        <div className="overlay flex  flex-col items-center">
+      <section className="home ">
+        <div className="home-container ">
 
           <article className="home-article1  " >
-            <h1 className="home-h1 text-xs  text-gray-400 text-center tracking-widest mb-2 lg:text-left">
-              SO, YOU WANT TO TRAVEL TO <span className=" home-h1-span block mt-3 lg:text-left ">SPACE</span>
+            <h1 className="home-h1 ">
+              SO, YOU WANT TO TRAVEL TO <span className=" home-h1-span ">SPACE</span>
             </h1>
-            <p className="  ">
+            <p className="home-p">
               Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
@@ -22,10 +23,10 @@ export const Home = () => {
             </p>
           </article>
 
-          <article className="home-article2 mt-20">
-            <button className=" rounded-full w-36 h-36 bg-white text-gray-800 font-bold animate-pulse  " onClick={handleClick} >
+          <article className="home-article2">
+            <button className="home-button " onClick={handleClick} >
                 <Link to='/destination'>EXPLORE</Link>
-            </button>
+            </button>00
           </article>
 
         </div>
